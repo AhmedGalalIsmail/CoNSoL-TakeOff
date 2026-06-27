d4c06b7eb65d55eb7cc1f89afd76f7fe16a3a6ad
AhmedGalalIsmail <ahmed.galal1977@yahoo.com>
2026-06-27 04:07:40 +0300

    Update assemblies and metadata for new build
    Rebuilt `Application.dll`, `Domain.dll`, `Infrastructure.dll`, and their corresponding `.pdb` files due to source code or project configuration changes.
    
    Updated `AssemblyInformationalVersionAttribute` in `Application.AssemblyInfo.vb` to reflect a new build/commit hash.
    
    Modified `Application.sourcelink.json` to point to the latest commit (`6c2c3e4a833be1d7e4d31887ca2a9dd01efabee2`).
    
    Updated `Application.AssemblyInfoInputs.cache` and `Application.vbproj.AssemblyReference.cache` to reflect changes in build inputs and assembly references.

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.vb
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("Application"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e1a538dae239389f4afac0369c4b54fb610f8dd"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c2c3e4a833be1d7e4d31887ca2a9dd01efabee2"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("Application"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("Application"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")> 
